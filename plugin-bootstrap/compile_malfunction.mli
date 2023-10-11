82f0d5085e1863dd3669c1b7af0c02bb12aaec14
Yannick Forster <yannick.forster@inria.fr>
2023-10-11 22:53:08 +0200

    fix bootstrap

--- plugin-bootstrap/compile_malfunction.mli
+++ plugin-bootstrap/compile_malfunction.mli
@@ -1,1 +1,1 @@
-val def_Malfunction_Pipeline_compile_malfunction : TemplateProgram.template_program -> Bytestring.String.t
+val compile_malfunction : TemplateProgram.template_program -> Bytestring.String.t
