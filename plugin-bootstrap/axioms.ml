38e20e86fe9853e94bd96ae80aab1fa937e837f7
Yannick Forster <yannick.forster@inria.fr>
2023-10-11 16:50:24 +0200

    update

--- plugin-bootstrap/axioms.ml
+++ plugin-bootstrap/axioms.ml
@@ -1,29 +1,29 @@
 
-let coq_Floats_PrimFloat_normfr_mantissa = Float64.normfr_mantissa
+let def_Coq_Floats_PrimFloat_normfr_mantissa = Float64.normfr_mantissa
 
-let coq_Floats_PrimFloat_frshiftexp = Float64.frshiftexp
+let def_Coq_Floats_PrimFloat_frshiftexp = Float64.frshiftexp
 
-let coq_Floats_PrimFloat_abs = Float64.abs
+let def_Coq_Floats_PrimFloat_abs = Float64.abs
 
-let coq_Floats_PrimFloat_ltb = fun f1 f2 -> not (Float64.lt f1 f2)
+let def_Coq_Floats_PrimFloat_ltb = fun f1 f2 -> not (Float64.lt f1 f2)
 
-let coq_Floats_PrimFloat_eqb = fun f1 f2 -> not (Float64.eq f1 f2)
+let def_Coq_Floats_PrimFloat_eqb = fun f1 f2 -> not (Float64.eq f1 f2)
 
-let coq_Floats_PrimFloat_div = Float64.div
+let def_Coq_Floats_PrimFloat_div = Float64.div
 
-let coq_Numbers_Cyclic_Int63_PrimInt63_lsr = Uint63.l_sr
+let def_Coq_Numbers_Cyclic_Int63_PrimInt63_lsr = Uint63.l_sr
 
-let coq_Numbers_Cyclic_Int63_PrimInt63_lsl = Uint63.l_sl
+let def_Coq_Numbers_Cyclic_Int63_PrimInt63_lsl = Uint63.l_sl
 
-let coq_Numbers_Cyclic_Int63_PrimInt63_land = Uint63.l_and
+let def_Coq_Numbers_Cyclic_Int63_PrimInt63_land = Uint63.l_and
 
-let coq_Numbers_Cyclic_Int63_PrimInt63_lor = Uint63.l_or
+let def_Coq_Numbers_Cyclic_Int63_PrimInt63_lor = Uint63.l_or
 
-let coq_Numbers_Cyclic_Int63_PrimInt63_sub = Uint63.sub
+let def_Coq_Numbers_Cyclic_Int63_PrimInt63_sub = Uint63.sub
 
-let coq_Numbers_Cyclic_Int63_PrimInt63_eqb f1 f2 = not (Uint63.equal f1 f2)
+let def_Coq_Numbers_Cyclic_Int63_PrimInt63_eqb f1 f2 = not (Uint63.equal f1 f2)
 
-let print_kername = fun x -> print_string (Metacoq_template_plugin.Caml_bytestring.caml_string_of_bytestring (snd x)) ; Printf.printf ("\n%!")
+(* let print_kername = fun x -> print_string (Metadef_Coq_template_plugin.Caml_bytestring.caml_string_of_bytestring (snd x)) ; Printf.printf ("\n%!") *)
 
 
 let rec print_obj x =
@@ -39,4 +39,4 @@ let rec print_obj x =
 
 let print_int = fun x -> Printf.printf ("%i\n%!") x
 
-let print = fun x -> print_string (Metacoq_template_plugin.Caml_bytestring.caml_string_of_bytestring x) ; Printf.printf ("\n%!")
+(* let print = fun x -> print_string (Metadef_Coq_template_plugin.Caml_bytestring.caml_string_of_bytestring x) ; Printf.printf ("\n%!") *)
