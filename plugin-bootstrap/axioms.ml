ac1f9550fc35e00b68440465cb250efe91af25bf
Yannick Forster <yannick.forster@inria.fr>
2023-10-14 13:51:06 +0200

    silent benchmarks

--- plugin-bootstrap/axioms.ml
+++ plugin-bootstrap/axioms.ml
@@ -11,8 +11,6 @@ let def_Coq_Floats_PrimFloat_eqb = fun f1 f2 -> not (Float64.eq f1 f2)
 
 let def_Coq_Floats_PrimFloat_div = Float64.div
 
-let def_Coq_Numbers_Cyclic_Int63_PrimInt63_leb = fun i1 i2 -> not (Uint63.le i1 i2)
-
 let def_Coq_Numbers_Cyclic_Int63_PrimInt63_lsr = Uint63.l_sr
 
 let def_Coq_Numbers_Cyclic_Int63_PrimInt63_lsl = Uint63.l_sl
@@ -23,22 +21,38 @@ let def_Coq_Numbers_Cyclic_Int63_PrimInt63_lor = Uint63.l_or
 
 let def_Coq_Numbers_Cyclic_Int63_PrimInt63_sub = Uint63.sub
 
+let def_Coq_Numbers_Cyclic_Int63_PrimInt63_leb = fun i1 i2 -> not (Uint63.le i1 i2)
+
 let def_Coq_Numbers_Cyclic_Int63_PrimInt63_eqb f1 f2 = not (Uint63.equal f1 f2)
 
 (* let print_kername = fun x -> print_string (Metadef_Coq_template_plugin.Caml_bytestring.caml_string_of_bytestring (snd x)) ; Printf.printf ("\n%!") *)
 
-
-let rec print_obj x =
+let rec print_obj x = 
+  let x = Obj.magic x in
   if Obj.is_block x then let size = Obj.size x in
-                           Printf.printf ("Block with tag %i of size %i\n%!") (Obj.tag x) (Obj.size x) ;
+                           if Obj.tag x = 247 then
+                              Printf.printf "POINTER%!"
+                           else
+                           (Printf.printf ("(block[%i] (tag %i) %!") (Obj.size x) (Obj.tag x) ;
                            for i = 0 to size - 1 do
                              print_obj (Obj.field x i)
-                           done
-  else  Printf.printf ("Integer: %c\n%!") (Obj.magic x)
+                           done;
+                           Printf.printf ")")
+  else  Printf.printf ("%i %!") x
+
+let def_MetaDef_Coq_VerifiedExtraction_Benchmarks_lib_vs_HELLO = fun _ x -> 
+  (* Printf.printf "Returning in map\n%!" ; print_obj x ; Printf.printf "\n%!" ; *) x
+
+let def_print_arg na x = ()
+  (* Printf.printf "Function %s called %!" na (\* ; print_obj x ; Printf.printf "\n%!" *\) *)
  (* = fun x -> Printf.printf ("%i\n%!") (Obj.tag x) ; Printf.printf ("%b\n%!") (Obj.is_block x) ; if Obj.is_block x then Printf.printf ("Size: %i\n%!") (Obj.size x) else () *)
 
 (* let print_obj = fun x -> Printf.printf ("%i\n%!") (Obj.tag x) ; Printf.printf ("%b\n%!") (Obj.is_block x) ; if Obj.is_block x then Printf.printf ("Size: %i\n%!") (Obj.size x) else () *)
 
 let print_int = fun x -> Printf.printf ("%i\n%!") x
+
+let def_print_string = fun x -> Printf.printf ("Global constant %s\n%!") x
+
+let print_hello = fun (x : unit) -> Printf.printf ("Hello\n%!")
 
 (* let print = fun x -> print_string (Metadef_Coq_template_plugin.Caml_bytestring.caml_string_of_bytestring x) ; Printf.printf ("\n%!") *)
