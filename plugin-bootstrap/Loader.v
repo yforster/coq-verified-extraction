a86a5a3521a04ed26d3af86da60f3e71fb3bd62a
Yannick Forster <yannick.forster@inria.fr>
2023-10-30 17:16:04 +0100

    fix mli

--- plugin-bootstrap/Loader.v
+++ plugin-bootstrap/Loader.v
@@ -2,3 +2,5 @@ Require Import String.
 From MetaCoq.Template Require ExtractableLoader.
 
 Declare ML Module "coq-metacoq-verified-malfunction.plugin".
+
+From Malfunction Require Export PrintMli.
--- theories/PrintMli.v
+++ theories/PrintMli.v
@@ -1,11 +1,10 @@
-From MetaCoq.Template Require Import Loader All Pretty.
-From MetaCoq.Utils Require Import bytestring.
 Require Import Nat List.
-
-Open Scope bs.
-Open Scope bool_scope.
-
 From Coq Require Import Strings.Ascii.
+From MetaCoq.Template Require Import Loader All Checker.
+From MetaCoq.Utils Require Import bytestring.
+
+Open Scope bool_scope.
+Open Scope bs.
 
 Definition newline : ascii := "010".
 Definition nl : string := String.String (byte_of_ascii newline) String.EmptyString.
@@ -21,23 +20,39 @@ Definition uncapitalize (s : bytestring.string) : bytestring.string :=
   | bytestring.String.String c s => bytestring.String.String (uncapitalize_char c) s
   end.
 
-Print BasicAst.context_decl.
+Definition capitalize_char (c : Byte.byte) : Byte.byte :=
+  let n := Byte.to_nat c in
+  if (97 <=? n)%nat && (n <=? 123)%nat then match Byte.of_nat (n - 32) with Some c => c | _ => c end
+  else c.
 
-Fixpoint print_type (t : term) :=
-  match t with
-  | tInd {| inductive_mind := (_, i) |} _ => uncapitalize i
-  | tProd _ A B =>
-      print_type A ++ " -> " ++ print_type B
-  | _ => "aaa"
+Definition capitalize (s : bytestring.string) : bytestring.string :=
+  match s with 
+  | bytestring.String.EmptyString => bytestring.String.EmptyString
+  | bytestring.String.String c s => bytestring.String.String (capitalize_char c) s
   end.
+
+Definition term_eqb (t1 t2 : term) :=
+  @eq_term config.default_checker_flags init_graph t1 t2.
+
+Notation "t === u" := (term_eqb t u) (at level 70).
 
 Fixpoint print_type_def def (t : term) :=
   match t with
   | tInd {| inductive_mind := (_, i) |} _ => uncapitalize i
-  | tProd {| binder_name := nAnon |} A B =>
-      print_type A ++ " -> " ++ print_type B
+  | tProd _ A B =>
+      print_type_def def A ++ " -> " ++ print_type_def def B
+  | tApp l [A] =>
+      if l === <% list %> then
+        print_type_def def A ++ " list"
+      else def
+  | tApp p [A;B] =>
+      if p === <% prod %> then
+        "(" ++ print_type_def def A ++ " * " ++ print_type_def def B ++ ")"
+      else def
   | _ => def
   end.
+
+Definition print_type := print_type_def "<NOTSUPPORTED>".
 
 Fixpoint print_types indna (ctx : context) :=
   match ctx with
@@ -47,7 +62,7 @@ Fixpoint print_types indna (ctx : context) :=
   end.
 
 Definition print_constructor indna  (na : ident) (ctx : context) :=
-  na ++
+  capitalize na ++
   match ctx with
   | [] => " "
   | l => " of " ++ print_types indna l
@@ -70,27 +85,33 @@ Definition print_inductive na (m : mutual_inductive_body) :=
 Fixpoint print_globals (Σ : global_declarations) :=
   match Σ with
   | [] => ""
-  | (na, InductiveDecl m) :: l => print_inductive na.2 m ++ nl ++
-                                                print_globals l
+  | (na, InductiveDecl m) :: l =>
+      if (na == (MPfile ["Datatypes"; "Init"; "Coq"], "list"))
+        || (na == (MPfile ["Datatypes"; "Init"; "Coq"], "prod"))
+      then print_globals l
+      else print_inductive na.2 m ++ nl ++
+             print_globals l
   | _ :: l => print_globals l
   end.
 
 Definition print_mli na (p : program) :=
-  print_globals p.1.(declarations) ++ nl ++ "val " ++ na ++ " : " ++ print_type p.2.
+  print_globals (MCList.rev p.1.(declarations)) ++ nl ++ "val " ++ na ++ " : " ++ print_type p.2.
 
 MetaCoq Quote Recursively Definition p := (nat -> bool).
-
 Import MCMonadNotation.
 
 Definition PrintMLI {A} (a : A) :=
+  A <- tmEval cbv A ;;
   p <- tmQuoteRec A ;;
   t <- tmQuote a ;;
   match t with
   | tConst kn _ =>
-      tmEval lazy (print_mli kn.2 p) >>= tmPrint
+      tmEval cbv (print_mli kn.2 p) >>= tmPrint
   | _ => tmFail "only constants supported"
   end.
 
+Definition test (u : nat * bool) := @nil (bool * nat).
+
 Notation "'Print' 'mli' x" := (PrintMLI x) (at level 0).
 
-MetaCoq Run Print mli Nat.ltb.
+MetaCoq Run Print mli test.
