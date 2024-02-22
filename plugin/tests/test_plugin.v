ebedd4ff1a744cf556c6f7cc03fbf34ea3964b18
Matthieu Sozeau <matthieu.sozeau@inria.fr>
2024-02-22 14:40:15 +0100

    Cleanup test_plugin

--- plugin/tests/test_plugin.v
+++ plugin/tests/test_plugin.v
@@ -7,12 +7,11 @@ Set MetaCoq Extraction Build Directory "_build".
 Definition test (x : unit) := coq_msg_info "Hello world!".
 
 Set Debug "metacoq-extraction".
-From Coq Require Import PrimFloat.
-
-Definition test_float (x : unit) : float := abs (-0.5%float).
 Set Warnings "-primitive-turned-into-axiom".
-MetaCoq Extraction -typed -unsafe -time -fmt -compile-plugin -run test_float "test_float.mlf".
 
+From Coq Require Import PrimFloat.
+Definition test_float (thunk : unit) : float := abs (-0.5%float).
+MetaCoq Extraction -typed -unsafe -time -fmt -compile-plugin -run test_float "test_float.mlf".
 
 (* Pure running time of the compiled code *)
 Time MetaCoq Eval test_float.
