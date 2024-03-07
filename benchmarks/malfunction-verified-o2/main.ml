c08e7b7e30a6f2ae12ac70c4b270bf2a39fc7cd8
Yannick Forster <yannick.forster@inria.fr>
2024-03-07 16:56:14 +0100

    use core_bench

--- benchmarks/malfunction-verified-o2/main.ml
+++ benchmarks/malfunction-verified-o2/main.ml
@@ -1,96 +1,24 @@
-open Printf
-open Unix
+open Core
+open Core_bench
 
-let demo0_main =
-  let n = int_of_string Sys.argv.(1) in
-  let t = Unix.gettimeofday () in
-  for i = 1 to n do
-     Demo0.demo0 ()
-  done;
-  let t' = Unix.gettimeofday () -. t in
-  Printf.printf "demo0 execution time: %f milliseconds\n" (t'*.1000.0)
-
-let demo1_main =
-  let n = int_of_string Sys.argv.(1) in
-  let t = Unix.gettimeofday () in
-  for i = 1 to n do
-    Demo1.demo1 ()
-  done;
-  let t' = Unix.gettimeofday () -. t in
-  Printf.printf "demo1 execution time: %f milliseconds\n" (t'*.1000.0)
-
-let demo2_main =
-  let n = int_of_string Sys.argv.(1) in
-  let t = Unix.gettimeofday () in
-  for i = 1 to n do
-    Demo2.demo2 ()
-  done;
-  let t' = Unix.gettimeofday () -. t in
-  Printf.printf "demo2 execution time: %f milliseconds\n" (t'*.1000.0)
-    
-let list_sum_main =
-  let n = int_of_string Sys.argv.(1) in
-  let t = Unix.gettimeofday () in
-  for i = 1 to n do
-     List_sum.list_sum ()
-  done;
-  let t' = Unix.gettimeofday () -. t in
-  Printf.printf "list_sum execution time: %f milliseconds\n" (t'*.1000.0)
-
-
-let vs_easy_main =
-  let n = int_of_string Sys.argv.(1) in
-  let t = Unix.gettimeofday () in
-  for i = 1 to n do
-    Vs_easy.vs_easy ()
-  done;
-  let t' = Unix.gettimeofday () -. t in
-  Printf.printf "vs_easy execution time: %f milliseconds\n" (t'*.1000.0)
-
-let vs_hard_main =
-  let n = int_of_string Sys.argv.(1) in
-  let t = Unix.gettimeofday () in
-  for i = 1 to n do
-    Vs_hard.vs_hard ()
-  done;
-  let t' = Unix.gettimeofday () -. t in
-  Printf.printf "vs_hard execution time: %f milliseconds\n" (t'*.1000.0)
-
-let binom_main =
-  let n = int_of_string Sys.argv.(1) in
-  let t = Unix.gettimeofday () in
-  for i = 1 to n do
-    Binom.binom ()
-  done;
-  let t' = Unix.gettimeofday () -. t in
-  Printf.printf "binom execution time: %f milliseconds\n" (t'*.1000.0)
-
-(* Color does not typecheck in OCaml *)
-let color_main =
-  let n = int_of_string Sys.argv.(1) in
-  let t = Unix.gettimeofday () in
-  for i = 0 to n do
-    Color.color ()
-  done;
-  let t' = Unix.gettimeofday () -. t in
-  Printf.printf "color xecution time: %f milliseconds\n" (t'*.1000.0)
-
-(* let sha_main =  
- *   let n = int_of_string Sys.argv.(1) in
- *   let t = Unix.gettimeofday () in
- *   for i = 1 to n do
- *     Sha.sha Tt
- *   done;
- *   let t' = Unix.gettimeofday () -. t in
- *   Printf.printf "sha execution time: %f seconds\n" t' *)
-
-let sha_fast_main =
-  let n = int_of_string Sys.argv.(1) in
-  let t = Unix.gettimeofday () in
-  for i = 1 to n do
-    (* Printf.printf "%i\n" (List.length ( *)
-        Sha_fast.def_MetaCoq_VerifiedExtraction_Benchmarks_lib_tests_sha_fast ()
-   (* ) ) *)
-  done;
-  let t' = Unix.gettimeofday () -. t in
-  Printf.printf "sha_fast execution time: %f milliseconds\n" (t'*.1000.0)
+let () =
+  Command.run (Bench.make_command [
+    Bench.Test.create ~name:"demo0"
+      (fun () -> ignore(Demo0.demo0 ()));
+    Bench.Test.create ~name:"demo1"
+      (fun () -> ignore(Demo1.demo1 ()));
+    Bench.Test.create ~name:"demo2"
+      (fun () -> ignore (Demo2.demo2 ()));
+    Bench.Test.create ~name:"list_sum"
+      (fun () -> ignore (List_sum.list_sum ()));
+    Bench.Test.create ~name:"vs_easy"
+      (fun () -> ignore ( Vs_easy.vs_easy ()));
+    Bench.Test.create ~name:"vs_hard"
+      (fun () -> ignore ( Vs_hard.vs_hard ()));
+    Bench.Test.create ~name:"binom"
+      (fun () -> ignore ( Binom.binom ()));
+    Bench.Test.create ~name:"color"
+      (fun () -> ignore (Color.color ()));
+    Bench.Test.create ~name:"sha_fast"
+      (fun () -> ignore ( Sha_fast.sha_fast ()));
+])
--- benchmarks/ocaml-noopt/main.ml
+++ benchmarks/ocaml-noopt/main.ml
@@ -1,98 +1,24 @@
-open Printf
-open Unix
+open Core
+open Core_bench
 
-let demo0_main =
-  let n = int_of_string Sys.argv.(1) in
-  let t = Unix.gettimeofday () in
-  for i = 1 to n do
-    Demo0.demo0 Tt
-  done;
-  let t' = Unix.gettimeofday () -. t in
-  Printf.printf "demo1 execution time: %f miliseconds\n" (t'*.1000.0)
-
-let demo1_main =
-  let n = int_of_string Sys.argv.(1) in
-  let t = Unix.gettimeofday () in
-  for i = 1 to n do
-    Demo1.demo1 Tt
-  done;
-  let t' = Unix.gettimeofday () -. t in
-  Printf.printf "demo1 execution time: %f miliseconds\n" (t'*.1000.0)
-  
-  
-let demo2_main =
-  let n = int_of_string Sys.argv.(1) in
-  let t = Unix.gettimeofday () in
-  for i = 1 to n do
-    Demo2.demo2 Tt
-  done;
-  let t' = Unix.gettimeofday () -. t in
-  Printf.printf "demo2 execution time: %f miliseconds\n" (t'*.1000.0)
-    
-
-let list_sum_main =
-  let n = int_of_string Sys.argv.(1) in
-  let t = Unix.gettimeofday () in
-  for i = 1 to n do
-    List_sum.list_sum Tt
-  done;
-  let t' = Unix.gettimeofday () -. t in
-  Printf.printf "list_sum execution time: %f miliseconds\n" (t'*.1000.0)
-
-
-let vs_easy_main =
-  let n = int_of_string Sys.argv.(1) in
-  let t = Unix.gettimeofday () in
-  for i = 1 to n do
-    Vs_easy.vs_easy Tt
-  done;
-  let t' = Unix.gettimeofday () -. t in
-  Printf.printf "vs_easy execution time: %f miliseconds\n" (t'*.1000.0)
-
-let vs_hard_main =
-  let n = int_of_string Sys.argv.(1) in
-  let t = Unix.gettimeofday () in
-  for i = 1 to n do
-    Vs_hard.vs_hard Tt
-  done;
-  let t' = Unix.gettimeofday () -. t in
-  Printf.printf "vs_hard execution time: %f miliseconds\n" (t'*.1000.0)
-
-
-let binom_main =  
-  let n = int_of_string Sys.argv.(1) in
-  let t = Unix.gettimeofday () in
-  for i = 1 to n do
-    Binom.binom Tt
-  done;
-  let t' = Unix.gettimeofday () -. t in
-  Printf.printf "binom execution time: %f miliseconds\n" (t'*.1000.0)
-
-
-(* Color does not typecheck in OCaml *)
-(* let color_main = *)
-(*   let n = int_of_string Sys.argv.(1) in *)
-(*   let t = Unix.gettimeofday () in *)
-(*   for i = 0 to n do *)
-(*     Color.color Tt *)
-(*   done; *)
-(*   let t' = Unix.gettimeofday () -. t in *)
-(*   Printf.printf "color xecution time: %f seconds\n" t' *)
-
-(* let sha_main =  
- *   let n = int_of_string Sys.argv.(1) in
- *   let t = Unix.gettimeofday () in
- *   for i = 1 to n do
- *     Sha.sha Tt
- *   done;
- *   let t' = Unix.gettimeofday () -. t in
- *   Printf.printf "sha execution time: %f seconds\n" t' *)
-
-let sha_fast_main =
-  let n = int_of_string Sys.argv.(1) in
-  let t = Unix.gettimeofday () in
-  for i = 1 to n do
-    Sha_fast.sha_fast Tt
-  done;
-  let t' = Unix.gettimeofday () -. t in
-  Printf.printf "sha_fast execution time: %f miliseconds\n" (t'*.1000.0)
+let () =
+  Command.run (Bench.make_command [
+    Bench.Test.create ~name:"demo0"
+      (fun () -> ignore(Demo0.demo0 Tt));
+    Bench.Test.create ~name:"demo1"
+      (fun () -> ignore(Demo1.demo1 Tt));
+    Bench.Test.create ~name:"demo2"
+      (fun () -> ignore (Demo2.demo2 Tt));
+    Bench.Test.create ~name:"list_sum"
+      (fun () -> ignore (List_sum.list_sum Tt));
+    Bench.Test.create ~name:"vs_easy"
+      (fun () -> ignore ( Vs_easy.vs_easy Tt));
+    Bench.Test.create ~name:"vs_hard"
+      (fun () -> ignore ( Vs_hard.vs_hard Tt));
+    Bench.Test.create ~name:"binom"
+      (fun () -> ignore ( Binom.binom Tt));
+    (* Bench.Test.create ~name:"color" *)
+    (*   (fun () -> ignore (Color.color Tt)); *)
+    Bench.Test.create ~name:"sha_fast"
+      (fun () -> ignore ( Sha_fast.sha_fast Tt));
+])
